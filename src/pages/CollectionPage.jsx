70ff274d6a009cf514119ac2647d08ae22839d52
Saumya Rawal <rawalsaumya22@gmail.com>
2026-01-05 12:21:32 +0530

    cards displaying in collection page

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -2,10 +2,17 @@ import { useSelector } from "react-redux"
 import CollectionCard from "../components/CollectionCard"
 const CollectionPage = () => {
     const collection = useSelector(state=>state.collection.items)
+    if(collection){
+        console.log("Hello collection")
+        console.log(collection)
+    }
     return (
         <div>
-            {collection.map[(item,idx)=>{
-                <div key={idx}><CollectionCard item={item}/></div>            }]}
+            {collection.map((item,idx)=>{
+                return <div key={idx}><CollectionCard item={item}/></div> 
+            }
+                )
+                }
         </div>
     )
 }
